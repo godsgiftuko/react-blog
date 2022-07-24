0f02f27a01f02bde9c75871830afea28f6f7ed2b
Godsgift Matthew <iamgodsgiftmatthew.com>
2022-07-24 11:37:55 +0100

    make it better

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,16 +1,22 @@
 import { useState, useRef } from 'react';
 // @mui
-import { styled } from '@mui/material/styles';
-import { Grid, Container } from '@mui/material';
+import { Container } from '@mui/material';
 // components
+import Page from '../components/Page';
 // sections
 
 export default function NotFound() {
 
     return (
-      <Page title="Coming Soon" background>
-          <Container>
-              <h1 styles="color: white">Not Found</h1>
+      <Page title="Not Found" >
+          <Container
+          sx={{  display: 'flex',
+          	justifyContent: 'center',
+          	alignItems: 'center',
+          	height: '100vh'
+      		}}
+          >
+              <h1>Not Found</h1>
           </Container>
     	</Page>
     );
